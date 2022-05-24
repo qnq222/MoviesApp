206a2fd5fdf0adb24ac8544ad4b679e531b38082
qnq222 <48182243+qnq222@users.noreply.github.com>
2022-05-24 10:12:20 +0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Ayman AbuMutair on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
